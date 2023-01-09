382e37bfa947df3e77e48681e8b62fe43ac44671
tech-world-code <64862958+tech-world-code@users.noreply.github.com>
2023-01-09 12:28:04 -0800

    Add files via upload
    Added first hundred files

---
+++ hello-world-RUST/target/debug/deps/hello_world_RUST-bb52210d3ec17030.d
@@ -0,0 +1,5 @@
+c:\Users\ekric\OneDrive\Desktop\Science Project Code\Non-exe\hello-world-RUST\target\debug\deps\hello_world_RUST-bb52210d3ec17030.rmeta: src\main.rs
+
+c:\Users\ekric\OneDrive\Desktop\Science Project Code\Non-exe\hello-world-RUST\target\debug\deps\hello_world_RUST-bb52210d3ec17030.d: src\main.rs
+
+src\main.rs:
---
+++ hello-world-RUST/target/debug/deps/hello_world_RUST-c9ee2a3c95f3208a.d
@@ -0,0 +1,5 @@
+c:\Users\ekric\OneDrive\Desktop\Science Project Code\Non-exe\hello-world-RUST\target\debug\deps\hello_world_RUST-c9ee2a3c95f3208a.rmeta: src\main.rs
+
+c:\Users\ekric\OneDrive\Desktop\Science Project Code\Non-exe\hello-world-RUST\target\debug\deps\hello_world_RUST-c9ee2a3c95f3208a.d: src\main.rs
+
+src\main.rs:
